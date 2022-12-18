2fa7605988cb4b3dd104be343d3a3f13ae29971f
Albrecht Oster <albrecht@oster.online>
2022-12-18 03:37:53 +0100

    Accept session invitation on control port

--- src/apple_midi/server.cr
+++ src/apple_midi/server.cr
@@ -4,6 +4,7 @@ require "./session"
 module AppleMidi
   BIND         = "::"
   CONTROL_PORT = 5099
+  PEER_NAME    = "pulseled"
 
   class Server
     def initialize
@@ -21,7 +22,7 @@ module AppleMidi
 
         case String.new(message[2..3])
         when "IN"
-          handle_invitation(message)
+          handle_invitation(message, client_addr)
         when "BY"
           handle_closing(message)
         end
@@ -32,19 +33,38 @@ module AppleMidi
       @socket.close
     end
 
-    private def handle_invitation(message)
+    private def handle_invitation(message, client_addr)
       session = @sessions.find { |s| s.initiator_token == message[8..11] }
       return unless session.nil?
 
-      session = Session.new(message)
+      session = Session.new(message, client_addr)
       @sessions << session
       puts "#{session.initiator_token.hexstring}: Created MIDI session with peer \"#{session.peer_name}\"."
+
+      accept_invitation(session, client_addr)
+    end
+
+    private def accept_invitation(session, client_addr)
+      io = IO::Memory.new
+
+      io.write_bytes(0xffff.to_u16)
+      io.write_string("OK".to_slice)
+      io.write_bytes(UInt32.new(2), IO::ByteFormat::NetworkEndian)
+      io.write(session.initiator_token)
+      io.write(session.ssrc)
+      io.write_string(PEER_NAME.to_slice)
+      io.write_byte(0)
+
+      session.peer_control_socket.send(io.to_slice)
+      puts "#{session.initiator_token.hexstring}: Accepted invitation."
+      puts "=> #{client_addr}"
     end
 
     private def handle_closing(message)
-      session = @sessions.find { |s| s.sender_ssrc == message[12..15] }
+      session = @sessions.find { |s| s.peer_ssrc == message[12..15] }
       return if session.nil?
 
+      session.close
       @sessions.delete(session)
       puts "#{session.initiator_token.hexstring}: Closed MIDI session."
     end
--- src/apple_midi/session.cr
+++ src/apple_midi/session.cr
@@ -1,13 +1,23 @@
 module AppleMidi
   struct Session
     getter initiator_token : Bytes
-    getter sender_ssrc : Bytes
+    getter peer_ssrc : Bytes
     getter peer_name : String
+    getter ssrc : Bytes
+    getter peer_control_socket
 
-    def initialize(invitation : Bytes)
+    def initialize(invitation : Bytes, client_addr)
       @initiator_token = invitation[8..11].dup
-      @sender_ssrc = invitation[12..15].dup
+      @peer_ssrc = invitation[12..15].dup
       @peer_name = String.new(invitation[16..])
+      @ssrc = Random.new.random_bytes(4)
+
+      @peer_control_socket = UDPSocket.new(Socket::Family::INET6)
+      @peer_control_socket.connect(client_addr)
+    end
+
+    def close
+      @peer_control_socket.close
     end
   end
 end
