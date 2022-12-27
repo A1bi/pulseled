405681708ab83302e65254a72796644da82bdb84
Albrecht Oster <albrecht@oster.online>
2022-12-27 22:38:17 +0100

    Make Perlin Noise moving directions configurable

--- src/effects/perlin_noise.cr
+++ src/effects/perlin_noise.cr
@@ -3,11 +3,18 @@ require "../led/color"
 
 module Effects
   class PerlinNoise < Effect
+    @[Flags]
+    enum MovingDirections
+      Horizontal
+      Vertical
+    end
+
     property colors : Array(Led::Color) = [Led::Color.red, Led::Color.blue]
     property max_brightness : Float64 = 1.0
     property offset_strips : Bool = true
     property speed : UInt8 = 1
     property step : Float32 = 0.075
+    property moving_directions : MovingDirections = MovingDirections::Horizontal
 
     def initialize(@led_strips)
       super
@@ -23,8 +30,15 @@ module Effects
       @perlin.x_offset = (offset_strips ? strip.size * i : 0).to_f32
 
       strip.size.times do |j|
-        position = j + beat
-        noise = @perlin.normalize_noise(position, 0, 0)
+        x = y = 0
+        if moving_directions.vertical?
+          x = j
+          y = beat
+        end
+        if moving_directions.horizontal?
+          x = j + beat
+        end
+        noise = @perlin.normalize_noise(x, y, y)
         alpha = [noise, 0.0].max * max_brightness
         apply_to_led(strip, j, color * alpha)
       end
