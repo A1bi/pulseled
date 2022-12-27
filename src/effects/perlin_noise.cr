f5d7ae31529b0ec9239d9e91b6b0a8c9654024d5
Albrecht Oster <albrecht@oster.online>
2022-12-27 21:50:28 +0100

    Add more configuration options to Perlin Noise effect

--- src/effects/perlin_noise.cr
+++ src/effects/perlin_noise.cr
@@ -5,17 +5,28 @@ module Effects
   class PerlinNoise < Effect
     property colors : Array(Led::Color) = [Led::Color.red, Led::Color.blue]
     property max_brightness : Float64 = 1.0
+    property offset_strips : Bool = true
+    property speed : UInt8 = 1
+    property step : Float32 = 0.075
+
+    def initialize(@led_strips)
+      super
+
+      @perlin = ::PerlinNoise.new
+      @perlin.step = step
+    end
 
     private def render_strip(strip, i, beat)
-      beat = beat_prescaler_steps(32)
-      perlin = ::PerlinNoise.new
+      beat = (beat * 30 * speed).to_i
       random = Random.new(i)
       color = colors.sample(random)
+      @perlin.x_offset = (offset_strips ? strip.size * i : 0).to_f32
 
-      strip.size.times do |i|
-        noise = perlin.normalize_noise((beat + i).to_i, 0, 0)
+      strip.size.times do |j|
+        position = j + beat
+        noise = @perlin.normalize_noise(position, 0, 0)
         alpha = [noise, 0.0].max * max_brightness
-        apply_to_led(strip, i, color * alpha)
+        apply_to_led(strip, j, color * alpha)
       end
     end
   end
